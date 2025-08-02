9e2d3cdd8f309b7654c142bc08f04e24bd5976da
Zahar <anthonymuncher@gmail.com>
2025-08-02 14:21:30 +0800

    fix typo anjaiii

--- lib/screens/language_settings_screen.dart
+++ lib/screens/language_settings_screen.dart
@@ -1,4 +1,4 @@
-yimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../l10n/app_localizations.dart';
 import '../services/language_service.dart';
